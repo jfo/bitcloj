94513a083de810b3a6ffe3feb678c25129e4e3f0
urthbound <jeffowler@gmail.com>
2014-04-19 16:57:53 -0400

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to bitcloj
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/bitcloj/core_test.clj
@@ -0,0 +1,7 @@
+(ns bitcloj.core-test
+  (:require [clojure.test :refer :all]
+            [bitcloj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
